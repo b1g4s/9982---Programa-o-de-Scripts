2b5aaaeb8631b727aa87f7ff133ca7c444e2e7d2
b1g4s <88548471+b1g4s@users.noreply.github.com>
2022-02-28 09:52:05 +0000

    28/02/2022

--- js/ProjetoPrimavera.js
+++ js/ProjetoPrimavera.js
@@ -1,16 +1,21 @@
 function primavera(){
-    const d = new Date();
-    const p = new Date(2022, 2, 20, 15, 33);
-    const fimMes = 28;
-
-    let dias = 28 - d.getDate() + p.getDate();
-    let hora = 0;
-
-    if (p.getHours() > d.getHours()) {
-        hora = p.getHours() - d.getHours(); 
+    const d=new Date();
+    const p=new Date(2022,2,20,15,33);
+    const fimmes=28;
+    let hora=0;
+    let minutos=0;
+    dias=28-d.getDate()+p.getDate();
+    if(p.getHours()>d.getHours()){
+        hora=p.getHours()-d.getHours();
     }
-
-    document.getElementById('hoje').innerHTML = d;
-    document.getElementById('primavera').innerHTML = p;
-    document.getElementById('hora').innerHTML = hora;
+    else{
+        hora=24-d.getHours()+p.getHours();
+    }
+    if(p.getMinutes()>d.getMinutes()){
+        minutos=p.getMinutes()-d.getMinutes();
+    } else{
+        minutos=60-d.getMinutes()+p.getMinutes();
+    }
+    let tempo="Faltam "+dias+" dias, "+hora+" horas, "+minutos+" minutos"+" para a primavera!";
+    document.getElementById('tempo').innerHTML=tempo; 
 }
